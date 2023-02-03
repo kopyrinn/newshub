2e423f95e4b26af2aef65abf425a0b7468fb7b3e
dimalebid <dima@lebid.pro>
2023-02-03 20:41:00 +0200

    web push

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -822,7 +822,7 @@ function getMoneyList(jQuery){
 
     <script type="module">
         import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
-        import { getMessaging, getToken } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging.js'
+        import { getMessaging, getToken, onMessage } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging.js'
 
         const app = initializeApp({
             apiKey: "AIzaSyD3JY3UVos0Xk1sk6VlTExFjpBXbsFNbW0",
@@ -891,20 +891,15 @@ function getMoneyList(jQuery){
             });
         }
 
-        // getToken(messaging, {vapidKey: 'BHZo1WgosZmyIphpVrJDFtCxW3x8_Ozxwwa-rf_iEAXSMUcLiWydBvP3OIqF--BCLjwYcv1zCl7xhMQzXGuchsU'}).then((currentToken) => {
-        //     if (currentToken) {
-        //         console.log(currentToken);
-        //         // Send the token to your server and update the UI if necessary
-        //         // ...
-        //     } else {
-        //         // Show permission request UI
-        //         console.log('No registration token available. Request permission to generate one.');
-        //         // ...
-        //     }
-        // }).catch((err) => {
-        //     console.log('An error occurred while retrieving token. ', err);
-        //     // ...
-        // });
+        onMessage(function(payload) {
+            const noteTitle = payload.notification.title;
+            const noteOptions = {
+                body: payload.notification.body,
+                icon: payload.notification.icon,
+            };
+            new Notification(noteTitle, noteOptions);
+        });
+
     </script>
 
 {{--    <script defer src="https://www.gstatic.com/firebasejs/3.9.0/firebase.js"></script>--}}
