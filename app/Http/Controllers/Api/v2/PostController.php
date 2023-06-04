6ce52607f0f847365f910f8967dabe6a68d8a690
Web Artisan <ad4ptec@gmail.com>
2023-06-04 17:08:02 +0300

    fix notify

--- app/Http/Controllers/Api/v2/PostController.php
+++ app/Http/Controllers/Api/v2/PostController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use App\Models\GrammaticalError;
 use App\Models\Post;
 use App\Models\Widget;
+use App\Notifications\ChannelNotification;
 use App\Notifications\NewPost;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
