964179d4709c7855cbb7d7be68d3020c7ce30acf
ad4ptec <ad4ptec@gmail.com>
2023-08-29 17:04:13 +0300

    fix thumbs

--- routes/console.php
+++ routes/console.php
@@ -27,20 +27,20 @@ Artisan::command('test', function () {
 
         if (!\Str::endsWith($user->avatar_sm, '.webp') || !Storage::disk('public')->exists($user->avatar_sm) || $user->avatar === $user->avatar_sm) continue;
         $path = Storage::disk('public')->path($user->avatar);
-        ImageOptimizer::optimize($path);
+        ImageOptimizer::optimize($path, $path);
         $path = Storage::disk('public')->path($user->avatar_sm);
-        ImageOptimizer::optimize($path);
+        ImageOptimizer::optimize($path, $path);
     }
 
     $polls = Poll::select('image', 'image_md', 'image_sm')->whereNotNull('image_sm')->orderBy('created_at', 'desc')->get();
     foreach ($polls as $i => $poll) {
         if (!\Str::endsWith($poll->image_md, '.webp') || !Storage::disk('public')->exists($poll->image_md) || $poll->image === $poll->image_md) continue;
         $path = Storage::disk('public')->path($poll->image_md);
-        ImageOptimizer::optimize($path);
+        ImageOptimizer::optimize($path, $path);
 
         if (!\Str::endsWith($poll->image_sm, '.webp') || !Storage::disk('public')->exists($poll->image_sm) || $poll->image === $poll->image_sm) continue;
         $path = Storage::disk('public')->path($poll->image_sm);
-        ImageOptimizer::optimize($path);
+        ImageOptimizer::optimize($path, $path);
     }
 
     $posts = Post::select('image', 'image_md', 'image_sm', 'image_fit')->whereNotNull('image_sm')->orderBy('created_at', 'desc')->get();
@@ -51,14 +51,14 @@ Artisan::command('test', function () {
 
         if (!\Str::endsWith($post->image_fit, '.webp') || !Storage::disk('public')->exists($post->image_fit) || $post->image === $post->image_fit) continue;
         $path = Storage::disk('public')->path($post->image_fit);
-        ImageOptimizer::optimize($path);
+        ImageOptimizer::optimize($path, $path);
 
         if (!\Str::endsWith($post->image_md, '.webp') || !Storage::disk('public')->exists($post->image_md) || $post->image === $post->image_md) continue;
         $path = Storage::disk('public')->path($post->image_md);
-        ImageOptimizer::optimize($path);
+        ImageOptimizer::optimize($path, $path);
 
         if (!\Str::endsWith($post->image_sm, '.webp') || !Storage::disk('public')->exists($post->image_sm) || $post->image === $post->image_sm) continue;
         $path = Storage::disk('public')->path($post->image_sm);
-        ImageOptimizer::optimize($path);
+        ImageOptimizer::optimize($path, $path);
     }
 });
