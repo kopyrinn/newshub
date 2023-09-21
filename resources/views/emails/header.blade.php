c0207900b00c5e18ef073baba65a73192e02f363
Ilyas Badyganov <iliyas_zhan@mail.ru>
2023-09-21 13:01:44 +0600

    fix email header

--- resources/views/emails/header.blade.php
+++ resources/views/emails/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
-<td class="header">
+<td class="header" align="center">
 <a href="{{ config('app.origin') }}" style="display: inline-block;">
-<img src="{{ config('app.origin') }}/logo-alt.webp" class="logo" alt="{{ trim($slot) }}">
+<img src="{{ config('app.origin') }}/logo-alt.webp" width="300" class="logo" alt="{{ trim($slot) }}">
 </a>
 </td>
 </tr>
